be137ee5a7de5f223d5ab2e302dc6d0c40211008
Aiacostata <gtata9469@gmail.com>
2025-04-21 14:54:49 -0300

    EX002

--- Exercicio002/QualoSeuNome.py
+++ Exercicio002/QualoSeuNome.py
@@ -1,2 +1,4 @@
+#Exercicio 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
 a = input("Qual o seu nome?")
-print("Olá " + a + ", prazer em conhecê-lo")
+#print("Olá " + a + ", prazer em conhecê-lo")
+print("Olá {}, é um prazer te conhecer!".format(a))
