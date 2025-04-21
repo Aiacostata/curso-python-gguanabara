5c0cbf5e3c048b8437e2296eb3b2820099ad4276
Aiacostata <gtata9469@gmail.com>
2025-04-21 14:33:02 -0300

    EX001

---
+++ Exercicio001/OlaMundo.py
@@ -0,0 +1,15 @@
+#Me sinto bobo, mas já revi esse começo de playlist 1000 vezes e nunca engrenei
+#Exercício 001 - Crie um programa que escreva "Olá, Mundo!" na tela.
+print("Hello World")
+a = "Hello"
+b = " "
+c = "World"
+#Tentei declarar a variavel 'a' como "string a = 'Hello' e naturalmente não deu certo, estou mal acostumado com o Java haha"
+print(a + b + c)
+#Testando isso...
+d = a + b + c
+print(d)
+#Mesma coisa...
+print("Olá, Mundo!")
+#Agora sim fazendo o que o exercício pede
+#Meu código tá muito sujo com esses comentários, vou fazer um arquivo separado para anotações
